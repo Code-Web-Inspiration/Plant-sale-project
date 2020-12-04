bcb896cbfc1689bea02f5d67824e566844a98973
Fampo Jonas <jojostra92@gmail.com>
2020-12-04 01:15:11 +0000

    Deuxième commit: Ajout des fichiers image [], module [header.css] et responsive [_@header.css]. Modification du fichier js [app.js]. TRAVAIL (mise en place du module header)

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@
 
 // JAVASCRIPT FILE
 
-const navBar = document.querySelector('#header-nav-bar')
-const navbarHeight = navBar.offsetHeight
+var navbarSlide = document.querySelector('.header-nav-list')
+// const navbarHeight = navBar.offsetHeight
 const menuButton = document.querySelector('#menu-button')
 
 const handleScrollY = function () {
@@ -13,8 +13,25 @@ const handleScrollY = function () {
     return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
 }
 
-function navbarStiky() {
-    navBar.classList.toggle('stiky', window.scrollY && handleScrollY() > navbarHeight)
-}
+// function navbarStiky() {
+//     navBar.classList.toggle('stiky', window.scrollY && handleScrollY() > navbarHeight)
+// }
 
-window.onload = () => {}
+window.onload = () => {
+    menuButton.addEventListener('click', function () {
+        switch (this.children[0].className) {
+            case 'web-site-menu-open-btn-component':
+                this.children[0].className = 'web-site-menu-close-btn-component'
+                navbarSlide.classList.add('slide')
+                break;
+            case 'web-site-menu-close-btn-component':
+                this.children[0].className = 'web-site-menu-open-btn-component'
+                navbarSlide.classList.remove('slide')
+                break;
+        
+            default:
+                this.children[0].className = 'web-site-menu-open-btn-component'
+                break;
+        }
+    }) 
+}
